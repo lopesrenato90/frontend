b79690862ebd6a422f648785695e2812ebc060ce
lopesrenato90 <blade.lopes@gmail.com>
2024-09-16 11:31:40 -0300

    Add files via upload

---
+++ MenuTest.java
@@ -0,0 +1,13 @@
+// Testing MenuFrame.
+import javax.swing.JFrame;
+
+public class MenuTest
+{
+   public static void main(String[] args)
+   { 
+      MenuFrame menuFrame = new MenuFrame(); 
+      menuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      menuFrame.setSize(500, 200); 
+      menuFrame.setVisible(true);
+   } 
+} // end class MenuTest
